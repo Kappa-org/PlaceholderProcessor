c2b9b1a746a543569e1c1e11c3c041aee9548e6d
Budry <zarubaondra@gmail.com>
2016-02-29 21:37:22 +0100

    Added invalid state exception

--- src/exceptions.php
+++ src/exceptions.php
@@ -1,3 +1,17 @@
 <?php
+/**
+ * This file is part of the Kappa\PlaceholderProcessor package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
 
 namespace Kappa\PlaceholderProcessor;
+
+/**
+ * Class InvalidStateException
+ * @package Kappa\PlaceholderProcessor
+ */
+class InvalidStateException extends \LogicException {}
