24979a4d4c21e4dcb8439e0499d5eacafbd42f98
Budry <zarubaondra@gmail.com>
2017-01-07 14:19:19 +0100

    Small refactoring

--- tests/KappaTests/bootstrap.php
+++ tests/KappaTests/bootstrap.php
@@ -7,12 +7,15 @@
  * For the full copyright and license information, please view the license.md
  * file that was distributed with this source code.
  */
+
 if ((!$loader = include __DIR__ . '/../../vendor/autoload.php') && (!$loader = include __DIR__ . '/../../../../autoload.php')) {
 	echo 'Install Nette Tester using `composer update --dev`';
 	exit(1);
 }
+
 /** @var Composer\Autoload\ClassLoader $loader */
 $loader->addPsr4("KappaTests\\", __DIR__);
+
 // configure environment
 Tester\Environment::setup();
 define('TEMP_DIR', __DIR__ . '/../temp/' . (isset($_SERVER['argv']) ? md5(serialize($_SERVER['argv'])) : getmypid()));
